9ced83731f2fbd004ca8220ea058d683de014000
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-11-09 20:35:10 +0530

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>This is none other than Home page in this webpage</h2>
+    </div>
+  );
+};
+
+export default Home;
